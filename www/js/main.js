0e11108cd87025cfd64fde00b3e8fbb74c9a833a
Cristian <brito.cristian@gmail.com>
2018-07-22 10:30:40 -0430

    www

---
+++ www/js/main.js
@@ -0,0 +1,18 @@
+; (function ($, window, document, undefined) {
+  'use strict';
+  $(function () {
+    $(document).ready(function () {
+
+      //  PARTIALS  //
+      
+      /**
+       * HOME
+      */
+      
+      console.log("Home partial");
+
+      
+
+    });
+  });
+})(jQuery, window, document);
